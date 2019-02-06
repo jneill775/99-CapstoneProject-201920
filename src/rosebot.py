9b7b97bcdc76b58b232aa6edc6f6d9454e260682
jneill775 <neilljs@rose-hulman.edu>
2019-02-06 16:26:20 -0500

    write the caw code for rasing and calib

--- src/rosebot.py
+++ src/rosebot.py
@@ -192,6 +192,11 @@ class ArmAndClaw(object):
 
     def raise_arm(self):
         """ Raises the Arm until its touch sensor is pressed. """
+        self.motor.turn_on(100)
+        while True:
+            if self.touch_sensor.is_pressed():
+                self.motor.turn_off()
+                break
 
     def calibrate_arm(self):
         """
@@ -202,6 +207,17 @@ class ArmAndClaw(object):
                (i.e., 14.2 motor revolutions),
           3. Resets the motor's position to 0.
         """
+        self.raise_arm()
+        self.motor.reset_position()
+        self.motor.turn_on(-100)
+        while True:
+            if abs(self.motor.get_position())==5112:
+                self.motor.turn_off()
+                self.motor.reset_position()
+                break
+
+
+
 
     def move_arm_to_position(self, desired_arm_position):
         """
