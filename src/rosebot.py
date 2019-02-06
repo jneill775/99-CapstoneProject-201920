7770c0b25a28da6c63fcf4c78aca24893052e481
jneill775 <neilljs@rose-hulman.edu>
2019-02-06 16:36:24 -0500

    wrote code to move arm to a desired position

--- src/rosebot.py
+++ src/rosebot.py
@@ -216,10 +216,22 @@ class ArmAndClaw(object):
                 self.motor.reset_position()
                 break
 
-
-
-
     def move_arm_to_position(self, desired_arm_position):
+
+        if desired_arm_position > self.motor.get_position():
+            self.motor.turn_on(100)
+            while True:
+                if (self.motor.get_position() == desired_arm_position):
+                    self.motor.turn_off()
+                    break
+        elif desired_arm_position < self.motor.get_position():
+            self.motor.turn_on(-100)
+            while True:
+                if (self.motor.get_position() == desired_arm_position):
+                    self.motor.turn_off()
+                    break
+        else:
+            pass
         """
         Move its Arm to the given position, where 0 means all the way DOWN.
         The robot must have previously calibrated its Arm.
