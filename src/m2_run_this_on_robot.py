067faa6a50ec10fe058715b06ff7e063b41a2314
lauterja <lauterja@rose-hulma.edu>
2019-02-07 16:17:28 -0500

    Wrote code to move the robot forward

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -8,7 +8,7 @@
 import rosebot
 import mqtt_remote_method_calls as com
 import time
-
+import shared_gui_delegate_on_robot as rec
 
 def main():
     """
@@ -16,7 +16,33 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    # run_test_arm()
+    # calibrate_arm()s
+    real_thing()
+def run_test_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.raise_arm()
 
+def calibrate_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
+
+def move_arm_to_position():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.move_arm_to_position()
+
+def lower_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.lower_arm()
+
+def real_thing():
+    robot = rosebot.RoseBot()
+    receiver = rec.Receiver(robot)
+    mqtt_reciver = com.MqttClient(receiver)
+    mqtt_reciver.connect_to_pc()
+
+    while True:
+        time.sleep(0.01)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
