f550a61113e4a660d13433f496159f19e78f1fe6
jneill775 <neilljs@rose-hulman.edu>
2019-02-07 16:02:59 -0500

    added gui for robot and implemented sender/reciever

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -8,7 +8,7 @@
 import rosebot
 import mqtt_remote_method_calls as com
 import time
-
+import shared_gui_delegate_on_robot as rec
 
 def main():
     """
@@ -16,7 +16,16 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    real_thing()
 
+def real_thing():
+    robot = rosebot.RoseBot()
+    reciever = rec.Reciever(robot)
+    mqtt_reciever = com.MqttClient(reciever)
+    mqtt_reciever.connect_to_pc()
+
+    while True:
+        time.sleep(0.01)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
