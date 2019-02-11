825b3ca521338f2f8bfbcbec20af88d8d9c5f6bb
lauterja <lauterja@rose-hulma.edu>
2019-02-10 19:53:54 -0500

    Wrote code for go straight

--- src/rosebot.py
+++ src/rosebot.py
@@ -87,6 +87,10 @@ class DriveSystem(object):
         Makes the robot go straight (forward if speed > 0, else backward)
         at the given speed for the given number of seconds.
         """
+        while True:
+            if time.time() - start >= seconds:
+                self.stop()
+                break
 
     def go_straight_for_inches_using_time(self, inches, speed):
         """
@@ -94,6 +98,9 @@ class DriveSystem(object):
         for the given number of inches, using the approximate
         conversion factor of 10.0 inches per second at 100 (full) speed.
         """
+        seconds_per_inch_at_100 = 10.0  # 1 sec = 10 inches at 100 speed
+        seconds = abs(inches * seconds_per_inch_at_100 / speed)
+        self.go_straight_for_seconds(seconds, speed)
 
     def go_straight_for_inches_using_encoder(self, inches, speed):
         """
