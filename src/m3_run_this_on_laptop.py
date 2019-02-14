0b021883630b9268a53fdd6a35a6ab2b884bf6f3
Yifei Xiao <xiaoy1@rose-hulman.edu>
2019-02-14 16:26:00 -0500

    add the LED gui

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -40,7 +40,7 @@ def main():
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame,are_frame,control_frame,drive_system_frame,sound_system_frame=get_shared_frames(main_frame,mqtt_sender)
+    teleop_frame,are_frame,control_frame,drive_system_frame,sound_system_frame,my_frame=get_shared_frames(main_frame,mqtt_sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
@@ -50,7 +50,7 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame,are_frame,control_frame,drive_system_frame, sound_system_frame)
+    grid_frames(teleop_frame,are_frame,control_frame,drive_system_frame, sound_system_frame,my_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -64,16 +64,45 @@ def get_shared_frames(main_frame, mqtt_sender):
     control_frame=shared_gui.get_control_frame(main_frame,mqtt_sender)
     drive_system_frame = shared_gui.get_drive_system_frame(main_frame, mqtt_sender)
     sound_system_frame = shared_gui.get_sound_system_frame(main_frame, mqtt_sender)
+    my_LED_frame = get_my_own_frame(main_frame, mqtt_sender)
 
-    return teleop_frame,arm_frame,control_frame,drive_system_frame,sound_system_frame
+    return teleop_frame,arm_frame,control_frame,drive_system_frame,sound_system_frame,my_LED_frame
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame,drive_system_frame, sound_system_frame):
+def grid_frames(teleop_frame, arm_frame, control_frame,drive_system_frame, sound_system_frame,my_frame):
     teleop_frame.grid(row=0,column=0)
     arm_frame.grid(row=1,column=0)
     drive_system_frame.grid(row=2,column=0)
     sound_system_frame.grid(row=3, column=0)
     control_frame.grid(row=4, column=0)
+    my_frame.grid(row=0, column=1)
+
+
+def get_my_own_frame(window,mqtt_sender):
+
+
+    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
+
+    LED_freq_label = ttk.Label(frame, text="LED Frequency:")
+    LED_freq_entry = ttk.Entry(frame, width=8)
+    LED_freq_button = ttk.Button(frame, text="Beep")
+
+
+    LED_freq_label.grid(row=0, column=1)
+    LED_freq_entry.grid(row=0, column=2)
+    LED_freq_button.grid(row=1, column=0)
+
+
+    LED_freq_button["command"] = lambda: handle_LED_freq_button(LED_freq_entry, mqtt_sender)
+
+    return frame
+
+def handle_LED_freq_button(LED_freq_entry, mqtt_sender):
+    print('LED shifting')
+    mqtt_sender.send_message("LEDfreq", [LED_freq_entry.get()])
+
+
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
