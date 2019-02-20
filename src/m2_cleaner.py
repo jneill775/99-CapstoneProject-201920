8c9a9bdaff588db98245e08a2af43a2585087794
lauterja <lauterja@rose-hulma.edu>
2019-02-20 12:47:49 -0500

    Sprint 3 almost there

---
+++ src/m2_cleaner.py
@@ -0,0 +1,80 @@
+import rosebot
+import time
+
+
+# This program will run a cleaning bot.
+
+
+def search():
+
+    robot = rosebot.RoseBot()
+    robot.led_system.left_led.turn_on()
+    robot.led_system.right_led.turn_on()
+    robot.drive_system.spin_counterclockwise_until_sees_object(25, 100)
+    robot.drive_system.go(50, 50)
+    while True:
+        d = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+        if d < 2:
+            robot.drive_system.stop()
+            robot.arm_and_claw.raise_arm()
+            break
+
+
+def put_away():
+
+    robot = rosebot.RoseBot()
+    robot.led_system.left_led.turn_on()
+    robot.led_system.right_led.turn_on()
+    robot.drive_system.spin_clockwise_until_sees_object(100, 25)
+    robot.drive_system.go(50,50)
+    while True:
+        d = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+        if d < 2:
+            robot.drive_system.stop()
+            robot.arm_and_claw.lower_arm()
+            robot.led_system.left_led.turn_off()
+            robot.led_system.right_led.turn_off()
+            break
+
+def celebration():
+
+    robot = rosebot.RoseBot()
+    robot.sound_system.speech_maker.speak('Cleaned Up')
+    robot.arm_and_claw.move_arm_to_position(2000)
+    robot.led_system.left_led.turn_on()
+    robot.led_system.right_led.turn_on()
+    time.sleep(.1)
+    robot.arm_and_claw.move_arm_to_position(1000)
+    robot.led_system.left_led.turn_off()
+    robot.led_system.right_led.turn_off()
+    time.sleep(.1)
+    robot.arm_and_claw.move_arm_to_position(2000)
+    robot.led_system.left_led.turn_on()
+    robot.led_system.right_led.turn_on()
+    time.sleep(.1)
+    robot.arm_and_claw.move_arm_to_position(1000)
+    robot.led_system.left_led.turn_off()
+    robot.led_system.right_led.turn_off()
+    time.sleep(.1)
+    robot.arm_and_claw.move_arm_to_position(2000)
+    robot.led_system.left_led.turn_on()
+    robot.led_system.right_led.turn_on()
+    time.sleep(.1)
+    robot.arm_and_claw.move_arm_to_position(0)
+    robot.led_system.left_led.turn_off()
+    robot.led_system.right_led.turn_off()
+
+
+
+def sleep():
+
+    robot = rosebot.RoseBot()
+    robot.led_system.left_led.turn_on()
+    robot.led_system.right_led.turn_on()
+    robot.sound_system.speech_maker.speak('Sleepy Time')
+    robot.drive_system.go_straight_for_inches_using_encoder(25,(50,50))
+    robot.drive_system.stop()
+    robot.arm_and_claw.raise_arm()
+    robot.led_system.left_led.turn_off()
+    robot.led_system.right_led.turn_off()
+
--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -30,6 +30,7 @@ def main():
     # -------------------------------------------------------------------------
     root = tkinter.Tk()
     root.title("CSSE 120 Super Epic Capstone Project *dabs*")
+    root.config(bg='red')
 
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
@@ -40,7 +41,7 @@ def main():
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, my_tones = get_shared_frames(main_frame, mqtt_sender)
+    # teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, my_tones = get_shared_frames(main_frame, mqtt_sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
@@ -50,75 +51,115 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, my_tones)
-
+    # grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, my_tones)
+    cleaner = cleaner_frame(main_frame, mqtt_sender)
+    cleaner.grid(row=2, column=3)
     # -------------------------------------------------------------------------
     # The event loop:
     # -------------------------------------------------------------------------
     root.mainloop()
 
 
-def get_shared_frames(main_frame, mqtt_sender):
-    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
-    arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
-    control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
-    drive_system_frame = shared_gui.get_drive_system_frame(main_frame, mqtt_sender)
-    sound_system_frame = shared_gui.get_sound_system_frame(main_frame, mqtt_sender)
-    tones_frame = my_tones(main_frame, mqtt_sender)
+# def get_shared_frames(main_frame, mqtt_sender):
+#     teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
+#     arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
+#     control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
+#     drive_system_frame = shared_gui.get_drive_system_frame(main_frame, mqtt_sender)
+#     sound_system_frame = shared_gui.get_sound_system_frame(main_frame, mqtt_sender)
+#     tones_frame = my_tones(main_frame, mqtt_sender)
+#
+#     return teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, tones_frame
 
-    return teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, tones_frame
 
+# def grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, my_tones):
+#     teleop_frame.grid(row=0, column=0)
+#     arm_frame.grid(row=1, column=0)
+#     drive_system_frame.grid(row=2, column=0)
+#     sound_system_frame.grid(row=3, column=0)
+#     control_frame.grid(row=4, column=0)
+#     my_tones.grid(row=0, column=1)
 
-def grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, my_tones):
-    teleop_frame.grid(row=0, column=0)
-    arm_frame.grid(row=1, column=0)
-    drive_system_frame.grid(row=2, column=0)
-    sound_system_frame.grid(row=3, column=0)
-    control_frame.grid(row=4, column=0)
-    my_tones.grid(row=0, column=1)
+# def my_tones(window, mqtt_sender):
+#
+#     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+#     frame.grid()
+#
+#     freq_label = ttk.Label(frame, text="Frequency:")
+#     freq_rate_label = ttk.Label(frame, text="Frequency Rate:")
+#     freq_entry = ttk.Entry(frame, width=8)
+#     freq_button = ttk.Button(frame, text="Frequency Beep")
+#     freq_rate_entry = ttk.Entry(frame, width=8)
+#     spin_clockwise_button = ttk.Button(frame, text="Spin Clockwise")
+#     spin_counterclockwise_button = ttk.Button(frame, text="Spin Counterclockwise")
+#     spin_clockwise_entry = ttk.Entry(frame, width=8)
+#     spin_counterclockwise_entry = ttk.Entry(frame, width=8)
+#
+#     freq_label.grid(row=0, column=1)
+#     freq_rate_label.grid(row=0, column=2)
+#     freq_entry.grid(row=1, column=1)
+#     freq_button.grid(row=1, column=0)
+#     freq_rate_entry.grid(row=1, column=2)
+#     spin_clockwise_button.grid(row=2, column=0)
+#     spin_counterclockwise_button.grid(row=3, column=0)
+#     spin_clockwise_entry.grid(row=2, column=1)
+#     spin_counterclockwise_entry.grid(row=3, column=1)
+#
+#     freq_button["command"] = lambda: handle_freq_button(freq_entry, freq_rate_entry, mqtt_sender)
+#     spin_clockwise_button["command"] = lambda: handle_clockwise(spin_clockwise_entry, mqtt_sender)
+#     spin_counterclockwise_button["command"] = lambda: handle_counterclockwise(spin_counterclockwise_entry, mqtt_sender)
+#
+#     return frame
 
-def my_tones(window, mqtt_sender):
+# def handle_freq_button(freq_entry, freq_rate_entry, mqtt_sender):
+#     print('Frequency beeping')
+#     mqtt_sender.send_message('frequency', [freq_entry.get(), freq_rate_entry.get()])
 
-    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+# def handle_clockwise(clockwise_entry, mqtt_sender):
+#     print('Spinning clockwise')
+#     mqtt_sender.send_message("right", [clockwise_entry.get(), clockwise_entry.get()])
+
+# def handle_counterclockwise(counter_entry, mqtt_sender):
+#     print('Spinning counterclockwise')
+#     mqtt_sender.send_message("left", [counter_entry.get(), counter_entry.get()])
+
+def cleaner_frame(window, mqtt_sender):
+
+    frame = tkinter.Frame(window, borderwidth=100, bg='red')
     frame.grid()
-
-    freq_label = ttk.Label(frame, text="Frequency:")
-    freq_rate_label = ttk.Label(frame, text="Frequency Rate:")
-    freq_entry = ttk.Entry(frame, width=8)
-    freq_button = ttk.Button(frame, text="Frequency Beep")
-    freq_rate_entry = ttk.Entry(frame, width=8)
-    spin_clockwise_button = ttk.Button(frame, text="Spin Clockwise")
-    spin_counterclockwise_button = ttk.Button(frame, text="Spin Counterclockwise")
-    spin_clockwise_entry = ttk.Entry(frame, width=8)
-    spin_counterclockwise_entry = ttk.Entry(frame, width=8)
-
-    freq_label.grid(row=0, column=1)
-    freq_rate_label.grid(row=0, column=2)
-    freq_entry.grid(row=1, column=1)
-    freq_button.grid(row=1, column=0)
-    freq_rate_entry.grid(row=1, column=2)
-    spin_clockwise_button.grid(row=2, column=0)
-    spin_counterclockwise_button.grid(row=3, column=0)
-    spin_clockwise_entry.grid(row=2, column=1)
-    spin_counterclockwise_entry.grid(row=3, column=1)
-
-    freq_button["command"] = lambda: handle_freq_button(freq_entry, freq_rate_entry, mqtt_sender)
-    spin_clockwise_button["command"] = lambda: handle_clockwise(spin_clockwise_entry, mqtt_sender)
-    spin_counterclockwise_button["command"] = lambda: handle_counterclockwise(spin_counterclockwise_entry, mqtt_sender)
+    frame_label = ttk.Label(frame, text='Cleaner Bot', font='Arial 18 bold', background='gray')
+    search_button = ttk.Button(frame, text='Search for toys!')
+    pick_up_button = ttk.Button(frame, text='Put away the toys')
+    celebrate_button = ttk.Button(frame, text='Celebrate the cleaning')
+    sleep_button = ttk.Button(frame, text='Sleepy time now')
+    frame_label.grid(row=0, column=2)
+    search_button.grid(row=3, column=1)
+    pick_up_button.grid(row=3, column=3)
+    celebrate_button.grid(row=3, column=2)
+    sleep_button.grid(row=4, column=2)
+    search_button["command"] = lambda: handle_search(mqtt_sender)
+    pick_up_button["command"] = lambda: handle_put_away(mqtt_sender)
+    celebrate_button["command"] = lambda: handle_celebrate(mqtt_sender)
+    sleep_button["command"] = lambda: handle_sleep(mqtt_sender)
 
     return frame
 
-def handle_freq_button(freq_entry, freq_rate_entry, mqtt_sender):
-    print('Frequency beeping')
-    mqtt_sender.send_message('frequency', [freq_entry.get(), freq_rate_entry.get()])
+def handle_search(mqtt_sender):
+    print("Go get them!")
+    mqtt_sender.send_message('search')
 
-def handle_clockwise(clockwise_entry, mqtt_sender):
-    print('Spinning clockwise')
-    mqtt_sender.send_message("right", [clockwise_entry.get(), clockwise_entry.get()])
+def handle_put_away(mqtt_sender):
+    print('Put them away!')
+    mqtt_sender.send_message('put_away')
 
-def handle_counterclockwise(counter_entry, mqtt_sender):
-    print('Spinning counterclockwise')
-    mqtt_sender.send_message("left", [counter_entry.get(), counter_entry.get()])
+def handle_celebrate(mqtt_sender):
+    print('Finally Done!')
+    mqtt_sender.send_message('celebration')
+
+def handle_sleep(mqtt_sender):
+    print('Goodnight')
+    mqtt_sender.send_message('sleep')
+
+
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
